f183aa422357e7e84a1d9eed420ad9215d12cec4
Lukas Jerusalem <lukasjerusalem@web.de>
2025-03-02 14:15:41 +0100

    Add Todo-Tasks

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Welcome to our Todolist App! \n")
+
+	fmt.Println("List of my Todos")
+	fmt.Println("Watch Go crash course")
+	fmt.Println("Programm app")
+	fmt.Println("Learn Java")
+	fmt.Println("Learn JavaScript")
+	fmt.Println("Watch the new season")
+
+	fmt.Println("Languages")
+	fmt.Println("Learn Java")
+	fmt.Println("Learn JavaScript")
+
+	fmt.Println("Rewards")
+	fmt.Println("Watch the new season")
+}
